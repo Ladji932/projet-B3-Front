a1080883304a7dbadbb5e89d87949c5bf05ccbf4
Ladji <ladjimaiga@Air-de-Ladji.lan>
2025-02-27 03:32:47 +0100

    modif

--- tech-titans/src/components/AdminLogin.jsx
+++ tech-titans/src/components/AdminLogin.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 const AdminLogin = ({ setToken }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
-  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,8 +20,8 @@ const AdminLogin = ({ setToken }) => {
       localStorage.setItem("adminToken", token); 
       console.log("Connexion réussie, token reçu :", token);
   
-      navigate("/#/admin");
-     } catch (err) {
+      window.location.hash = "#/admin";
+       } catch (err) {
       setError(err.response?.data?.message || "Erreur d'authentification.");
     }
   };
